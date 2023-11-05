3ed96eb93acab4d1d221032f7f41f976f19c9209
Cristian Perafan <1110282139@u.icesi.edu.co>
2023-11-04 21:54:32 -0500

    Feat:Add h2 configuration with yml file and UserRepository impl

--- src/main/java/com/cristian/JsonWebToken/repository/persistence/UserRepository.java
+++ src/main/java/com/cristian/JsonWebToken/repository/persistence/UserRepository.java
@@ -1,4 +1,10 @@
 package com.cristian.JsonWebToken.repository.persistence;
 
-public class UserRepository {
+import com.cristian.JsonWebToken.repository.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByUsername(String username);
 }
